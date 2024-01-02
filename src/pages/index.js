e4bd810c1e69636410a50572e9c0397ca911be85
Lhamjav Sanjmyatav <lkhamjavsanjmytav@gmail.com>
2024-01-02 19:15:04 +0800

    mobile

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,14 +15,14 @@ export default function Home() {
 
   return (
     <main id='main'>
-      <Head>
+      {/* <Head>
         <title>protofolio lkhamjav | </title>
         <meta
           name="description"
           content="lkhamjav protofolio."
           key="desc"
         />
-      </Head>
+      </Head> */}
       <Header />
       <Steph />
       <About />
