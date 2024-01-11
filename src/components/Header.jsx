4188babdde1b87549f1f0e9316dfdb2b120b1646
Lhamjav Sanjmyatav <lkhamjavsanjmytav@gmail.com>
2024-01-11 17:35:05 +0800

    hamburger menu activate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,28 +23,14 @@ export const Header = () => {
     };
     return (
         <div>
-            <header className=" bg-white dark:bg-black flex justify-center" >
-                <div className='flex items-center lg:px-20 py-4 justify-between w-3/4'>
-                    <h1 className="lg:text-3xl font-bold leading-9 tracking-wide dark:text-white  ">
+            <header className=" bg-white dark:bg-black flex justify-center items-center" >
+                <div className='flex  lg:px-20 py-4 justify-between w-3/4'>
+                    <h1 className="lg:text-3xl font-bold leading-9 tracking-wide dark:text-white relative ">
                         {"<SS />"}
-                    </h1>
-                    <nav className="hidden lg:flex content-center items-center gap-6 text-gray-900 dark:text-white text-base font-medium leading-6 ">
-                        <a>About</a>
-                        <a>Work</a>
-                        <a>Testimonials</a>
-                        <a>Contact</a>
-                        <img onClick={toggleTheme} src={`${theme === 'light' ? './sun.svg' : './moon.svg'}`} className='sun' id='sun'></img>
-                        <img src="./moon.svg" alt="" className='moon hidden' />
-                        <button className="lg:rounded-xl bg-gray-900  text-gray-50 dark:bg-[#F9FAFB] dark:text-[#111827] text-base font-medium leading-6 py-1 px-4 ">
-                            Download CV
-                        </button>
-                    </nav>
-                    <nav className='lg:hidden'>
-                        <img onClick={toggleBurger} src={`${burger === 'menu' ? './menu.svg' : './xmark.png'}`} alt="" />
-                        <div className={`${burger === 'menu' ? 'hidden ' : 'flex'} absolute`}>
-                            <div className='flex items-center justify-center  dark:bg-black '>
+                        <div className={`${burger === 'menu' ? 'hidden ' : 'flex'} absolute  justify-center bg-white dark:bg-black items-start`}>
+                            <div className='flex items-start justify-center  dark:bg-black h-[820px] w-[300px] '>
                                 <hr className='bg-[#e5e7eb] dark:bg-[#374151]' />
-                                <nav className="flex flex-col  gap-6 text-gray-900 dark:text-white text-base font-medium leading-6 w-3/4 ">
+                                <nav className="flex flex-col  gap-6 text-gray-900 dark:text-white text-basefont-medium leading-6 w-3/4 ">
                                     <a>About</a>
                                     <a>Work</a>
                                     <a>Testimonials</a>
@@ -62,6 +48,20 @@ export const Header = () => {
                                 </nav >
                             </div >
                         </div>
+                    </h1>
+                    <nav className="hidden lg:flex content-center items-center  gap-6 text-gray-900 dark:text-white text-base font-medium leading-6 ">
+                        <a>About</a>
+                        <a>Work</a>
+                        <a>Testimonials</a>
+                        <a>Contact</a>
+                        <img onClick={toggleTheme} src={`${theme === 'light' ? './sun.svg' : './moon.svg'}`} className='sun' id='sun'></img>
+                        <img src="./moon.svg" alt="" className='moon hidden' />
+                        <button className="lg:rounded-xl bg-gray-900  text-gray-50 dark:bg-[#F9FAFB] dark:text-[#111827] text-base font-medium leading-6 py-1 px-4 ">
+                            Download CV
+                        </button>
+                    </nav>
+                    <nav className='lg:hidden '>
+                        <img onClick={toggleBurger} src={`${burger === 'menu' ? './menu.svg' : './xmark.png'}`} alt="" />
                     </nav>
 
                 </div>
